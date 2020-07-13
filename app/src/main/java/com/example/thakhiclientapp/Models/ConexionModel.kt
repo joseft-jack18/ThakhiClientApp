63e59ef8906e4bbf6f72aba4c41f1c78b0df1750
NILTON EDY PEREZ MAMANI <nilperezm@upt.pe>
2020-07-13 11:17:46 -0500

    Diseño

--- app/src/main/java/com/example/thakhiclientapp/Models/ConexionModel.kt
+++ app/src/main/java/com/example/thakhiclientapp/Models/ConexionModel.kt
@@ -3,7 +3,7 @@ package com.example.thakhiclientapp.Models
 class ConexionModel {
     companion object{
         //var url:String="https://thakhi.club/Cliente/"
-        var url:String="http://192.168.1.38/Cliente/"
+        var url:String="http://192.168.1.6/Cliente/"
         //var url:String="http://192.168.1.44:8080/ThakhiWS/Cliente/"
         var dniCliente:String=""
         var nombreCliente:String=""
