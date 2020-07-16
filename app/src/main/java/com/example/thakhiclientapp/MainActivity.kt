f1b555c87dc2edd6d15a0a6c74c3caca6cd9379a
NILTON EDY PEREZ MAMANI <nilperezm@upt.pe>
2020-07-15 22:23:56 -0500

    Mapa V1

--- app/src/main/java/com/example/thakhiclientapp/MainActivity.kt
+++ app/src/main/java/com/example/thakhiclientapp/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var saludo:String="Hola messi mierra"
+        var saludo:String="Hola"
     }
 }
